ae4631a26d20d63c53522285fd5b46183c2eb42a
jirapan <chayen.chy@hotmail.com>
2018-06-13 18:05:28 +0700

    todos

---
+++ todos/src/app/todo/MyFilterPiple.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';  
+import { TodoStore } from '../todo.service';  
+  
+@Pipe({  
+    name: 'myfilter',  
+    pure: false  
+})  
+  
+export class MyFilterPipe implements PipeTransform {  
+    transform(items: any[], filter: any): any {  
+        if (!items || !filter) {  
+            return items;  
+        }  
+        //debugger;
+        return filter.all ? items : items.filter((i) => {return i.completed === filter.completed;})
+    }  
+}  
