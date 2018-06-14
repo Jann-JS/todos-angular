d307ba50fe2b0d77cde43ca791d34d51e8f20945
jirapan <chayen.chy@hotmail.com>
2018-06-14 11:38:11 +0700

    two-commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,24 +8,24 @@ import { Todo, TodoStore } from '../todo.service';
 })
 export class TodoComponent implements OnInit {
 
-  ngOnInit() {
-  }
-  
-  	head_title = 'todos';
+	head_title = 'todos';
   	todoStore: TodoStore;
 	newTodoText = '';
-	
 	filterargs = {};
+	clickCompleted;clickActive;clickAll;
 
+	ngOnInit() {
+	}
+  
 	constructor(
 		todoStore: TodoStore,
 		private changeTodo: ChangeDetectorRef
 	) {
-		this.todoStore = todoStore;   // ******
+		this.todoStore = todoStore; 
 		this.checkOnclick('all');
 	}
   
-	addTodo(text: string) {
+	public addTodo(text: string) {
 		this.newTodoText = text;
 		if(this.newTodoText) {
 		this.todoStore.add(this.newTodoText);
@@ -34,16 +34,16 @@ export class TodoComponent implements OnInit {
 		//this.checkOnclick('all');
 	}
   
-  	stopEditing(todo: Todo, editedTitle: string) {
+  	private stopEditing(todo: Todo, editedTitle: string) {
 		todo.title = editedTitle;
 		todo.editing = false;
 	}
 
-	cancelEditing(todo: Todo) {
+	private cancelEditing(todo: Todo) {
 		todo.editing = false;
 	}
 
-	updateEditing(todo: Todo, editedTitle: string) {
+	public updateEditing(todo: Todo, editedTitle: string) {
 		editedTitle = editedTitle.trim();
 		todo.editing = false;
 
@@ -55,26 +55,23 @@ export class TodoComponent implements OnInit {
 		this.todoStore.updateStore();
 	}
 
-	editTodo(todo: Todo) {
+	private editTodo(todo: Todo) {
 		todo.editing = true;
 	}
 
-	removeCompleted() {
+	public removeCompleted() {
 		this.todoStore.removeCompleted();
 	}
 
-	toggleCompletion(todo: Todo) {
+	public toggleCompletion(todo: Todo) {
 		this.todoStore.toggleCompletion(todo);
 	}
 
-	remove(todo: Todo){
+	public remove(todo: Todo){
 		this.todoStore.remove(todo);
 	}
 
-	clickCompleted;clickActive;clickAll;
-	statusSelect;
-	allStorage = [];
-	checkOnclick(status: string) {
+	public checkOnclick(status: string) {
 		//console.log(status);
 		switch(status) {
 			case 'completed': { 
