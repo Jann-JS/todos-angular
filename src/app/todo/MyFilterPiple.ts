9eec1ae9c10400f000422502f5d2b0de49d0d5d0
jirapan <chayen.chy@hotmail.com>
2018-06-14 14:44:11 +0700

    Fix3

--- src/app/todo/MyFilterPiple.ts
+++ src/app/todo/MyFilterPiple.ts
@@ -1,16 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';  
-import { TodoStore } from '../todo.service';  
-  
-@Pipe({  
-    name: 'myfilter',  
-    pure: false  
-})  
-  
-export class MyFilterPipe implements PipeTransform {  
-    transform(items: any[], filter: any): any {  
-        if (!items || !filter) {  
-            return items;  
-        }  
-        return filter.all ? items : items.filter((i) => {return i.completed === filter.completed;})
-    }  
-}  
+import { Pipe, PipeTransform } from '@angular/core';
+import { TodoStore } from '../todo.service';
+
+@Pipe({
+    name: 'myfilter',
+    pure: false
+})
+
+export class MyFilterPipe implements PipeTransform {
+    transform(items: iItems[], filter: any): string {
+        if (!items || !filter) {
+            return items;
+        }
+        return filter.all ? items : items.filter((i) => { return i.completed === filter.completed;})
+    }
+}
+
+interface iItems {
+    completed: boolean;
+    editing: boolean;
+    _title: boolean;
+}
